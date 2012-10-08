0ed11a5f5ed874f9c23013d8d71dda09817c18d4
Evan Solomon <evan@evanalyze.com>
2012-10-07 19:07:02 -0700

    Load general.php in init.php

--- init.php
+++ init.php
@@ -1,7 +1,8 @@
 <?php
 
 // Load everything
-include( __DIR__ . '/db.php'               );
-include( __DIR__ . '/objects.php'          );
-include( __DIR__ . '/functions/game.php'   );
-include( __DIR__ . '/functions/season.php' );
+include( __DIR__ . '/db.php'                );
+include( __DIR__ . '/objects.php'           );
+include( __DIR__ . '/functions/general.php' );
+include( __DIR__ . '/functions/game.php'    );
+include( __DIR__ . '/functions/season.php'  );
