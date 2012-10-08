b386c30950bda3344ae9fceed21b598c8a2ef1bb
Evan Solomon <evan@evanalyze.com>
2012-10-07 19:00:03 -0700

    Rename games.php to game.php
    Consistent style

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php
 
 // Load everything
-include( __DIR__ . '/db.php'              );
-include( __DIR__ . '/objects.php'         );
-include( __DIR__ . '/functions/games.php' );
+include( __DIR__ . '/db.php'               );
+include( __DIR__ . '/objects.php'          );
+include( __DIR__ . '/functions/game.php'   );
+include( __DIR__ . '/functions/season.php' );
