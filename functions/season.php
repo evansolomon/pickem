e903a01beecfcd58bccc7b2750569e916829d90b
Evan Solomon <evan@evanalyze.com>
2012-10-07 20:14:24 -0700

    Get season stats through the given week

--- functions/season.php
+++ functions/season.php
@@ -4,7 +4,7 @@
 class Season_Data {
 	static function get_wins( $season, $week, $team ) {
 		$wins = 0;
-		for ( $i = 1; $i < $week; $i++ ) {
+		for ( $i = 1; $i <= $week; $i++ ) {
 			$game = get_game( $season, $i, $team );
 			if ( $team == Game_Data::get_winner( $game )->name )
 				$wins++;
@@ -15,7 +15,7 @@ class Season_Data {
 
 	static function get_losses( $season, $week, $team ) {
 		$wins = 0;
-		for ( $i = 1; $i < $week; $i++ ) {
+		for ( $i = 1; $i <= $week; $i++ ) {
 			$game = get_game( $season, $i, $team );
 			if ( $team == Game_Data::get_winner( $game )->name )
 				$wins++;
@@ -33,7 +33,7 @@ class Season_Data {
 
 	static function get_stat( $season, $week, $team, $stat ) {
 		$total = 0;
-		for ( $i = 1; $i < $week; $i++ ) {
+		for ( $i = 1; $i <= $week; $i++ ) {
 			$game   =  get_game( $season, $i, $team );
 			$winner =  Game_Data::is_winner( $game, $team );
 			$total  += Game_Data::get_stat( $game, $stat, $winner );
